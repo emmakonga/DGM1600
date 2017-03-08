2e17c3db2a5537d195988e1c95b72d41071f327d
Unknown <emmanymakonga@gmail.com>
2017-03-08 11:24:24 -0700

    In Class_CatList

---
+++ DGM1600_Assignments/Assets/Scripts/List.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List : MonoBehaviour {
+
+	public List<string> cat;
+
+	// Use this for initialization
+	public void Start () {
+		cat.Add("Fat cat");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
