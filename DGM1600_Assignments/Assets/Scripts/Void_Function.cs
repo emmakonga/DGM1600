8b659b60e102871a273bca1116ac3fe5d1c4f686
Unknown <emmanymakonga@gmail.com>
2017-04-07 11:39:54 -0600

    Fiinish Void Function

--- DGM1600_Assignments/Assets/Scripts/Void_Function.cs
+++ DGM1600_Assignments/Assets/Scripts/Void_Function.cs
@@ -3,119 +3,208 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Void_Function : MonoBehaviour {
-	public int weapon = 0;
-	public int function;
-	public string deepThought;
-	//private int MyFunction (bool myBool);
-	public int StrangeFunction;
+	//Declares public int variable
+	public int num1 = 5;
+	//Declares public int variable
+	public int num2 = 3;
+	//Declares public int variable
+	public int num3 = 15;
+	//Declares public array of strings called animals
+	public string[] animals; 
+	//Declares public string
+	public string text = "Bonjours";
+	//Declares public int variable
+	public int num4 = 20;
+	//Declares public array of strings called cars
+	public string[] cars; 
+	//Declares public int variable
+	//Declares public string
+	public string text1 = "Hello World";
 
-//	// start first example
+	public List<string> furniture;
 
-//	{
-//		// Update is called once per frame
+	void Start()
+	{
+		//Initializes array to size 3
+		animals = new string[3];
+		//Assigns animals to the array
+		animals[0] = "Cat";
+		animals[1] = "Dog";
+		animals[2] = "Sheep";
+		//Done assigning animals
 
-//	}
-//		if (Input.GetKey (KeyCode.Space))
-//	{
-//		WeaponSearch ();
-//	}
-//		void WeaponSearch()
-//	{
-//		weapon = Random.Range (1,10);
-//		switch(weapon)
-//	{
-//		case 1:
-//		print("I got a gun");
-//		break;
-//
-//		case 2:
-//			print("I got a chain");
-//			break;
-//
-//		case 3:
-//			print("I got a knife");
-//			break;
-//		case 4: 
-//			print("I got an hamer");
-//			break;
-//
-//		//Default:
-//		//print("I didn't find any weapon!");
-//		//break;
-//
-//		//end first example
-//	}
-//	
-//
-//		//start second example
-//
-//		void Start ()
-//		{
-//		int myInt = 5;
-//
-//		void Start (){
-//	
-//		myInt = 55;
-//		print (myInt * 2);
-//
-//		}
-//
-//		int MultiplyByTwo(int number){
-//		int ret;
-//		return ret;
-//
-//		//end second example
-//			
-//		//start third example
-//		void start(bool myBool)
-//		{
-//		if (myBool)
-//		return 2;
-//		else
-//		return -2;
-//		}
-//		//end third example
-//
-//		//start fourth example
-//		void StrangeFunction(int x, out y, out z) {
-//		y = x * 2;
-//		z = x / 2;
-//		}
-//
-//		void Start()
-//		{
-//		int x = 5;
-//		MultiplyXByTwo(ref x);
-//		print(x); 
-//
-//		int y; 
-//		int z;
-//		StrangeFunction(x, out y, out z);
-//		print(x);
-//		print(y); 
-//		print(z); 
-//		}
-//		//end fourth example
-//
-//		//start second example
-//
-//		GameObject player;
-//		void Start () {
-//		player = GameObject.FindWithTag("Player");
-//		}
-//
-//
-//		/////////***** Change to C# *****
-//		 player : GameObject;
-//				function Start () {
-//					player = GameObject.FindWithTag("Player");
-//				}
-//
-//		//end second example
-//
-//
-//	}
-//
-//
-//		
+		//Initializes array to size 3
+		cars = new string[2];
+		//Assigns cars to the array
+		cars[0] = "Nissan";
+		cars[1] = "Toyota";
+		//Done assigning cars
+
+		//Calls the "SayHello" function
+		SayHello ();
+		//Calls the "AddNumbers" function
+		AddNumbers (num1, num2);
+		//Calls the "Average" function
+		Average ();
+		//Calls the "Subtract" function
+		Subtract (num3, num1);
+		//Calls the "ListAnimals" function
+		ListAnimals (animals);
+		//Calls the "Bonjours" function
+		Bonjours();
+		//Calls the "MultiplyNumbers"function
+		MultiplyNumbers(num1, num2, num3, num4);
+		//Calls the "ListCars" function
+		ListCars (cars);
+		//Calls the "Hello World" function
+		HelloWorld ();
+		FurnitureList (furniture);
+	}
+
+
+
+	//Start first example
+
+	//Creates a void function called SayHello
+	void SayHello ()
+	{
+		//prints "Hello"
+		print ("Hello!");
+	}
+	//End first example
+
+	//Start second example
+
+	//Creates a void function called AddNumbers that takes 2 int parameters
+	void AddNumbers(int a, int b)
+	{
+		//Declares temporary int variable sum
+		int sum;
+		//Sets sum equal to the sum of a + b
+		sum = a + b;
+		//Prints the sum
+		print ("Sum: " + sum);
+	}
+	//End second example
+
+	//Start third example
+	//Creates a void function called Average
+	void Average()
+	{
+		//Declares float variable mean equal to num1 + num2 / 2
+		float mean = num1 + num2 / 2;
+		//Prints the average
+		print ("Average: " + mean);
+	}
+	//End third example
+
+	//Start fourth example
+	//Creates a void function called Subtract that takes 2 int parameters
+	void Subtract(int a, int b)
+	{
+		//Declares int variable diff
+		int diff;
+		//sets diff equal to a - b
+		diff = a - b;
+		//Prints difference
+		print ("Difference: " + diff);
+	}
+	//End fourth example
+
+	//Start fifth example
+	//Creates a void function called ListAnimals that takes a string[] parameter
+	void ListAnimals(string[] myArray)
+	{
+		print ("My Animals: ");
+		//Loops through array passed into function
+		for (int i=0; i<myArray.Length; i++)
+		{
+			//Prints the animal in each array element
+			print(myArray[i]);
+		}
+	}
+	//End fifth example
+
+	//Start sixth example
+
+	//Creates a void function called Bonjours
+	void Bonjours ()
+	{
+		//prints "Bonjours"
+		print ("Bonjours");
+	}
+	//End sixth example
+
+	//Start seventh example
+	//Creates a void function called MultiplyNumbers that takes 4 int parameters
+	void MultiplyNumbers(int c, int d, int e, int f)
+	{
+		//Declares int variable sum
+		int produit;
+		//sets sum equal to c * d * e
+		produit = c * d * e * f;
+		//Prints difference
+		print ("Produit: " + produit);
+	}
+	//End seventh example
+
+	//Start eigth example
+	//Creates a void function called ListCars that takes a string[] parameter
+	void ListCars(string[] myArray)
+	{
+		print ("My Cars: ");
+		//Loops through array passed into function
+		for (int i=0; i<myArray.Length; i++)
+		{
+			//Prints the animal in each array element
+			print(myArray[i]);
+		}
+	}
+	//End eigth example
+
+	//Start ninenth example
+
+	//Creates a void function called "Hello World"
+	void HelloWorld ()
+	{
+		//prints "Hello World"
+		print ("HelloWorld");
+	}
+	//End ninenth example
+
+	//start tenth example
+
+
+	////Creates a void function called ArrayList that takes a string[] parameter
+	void FurnitureList (List<string> myList)
+	{
+		//Adding items to List
+		myList.Add ("Chair");
+		myList.Add ("Bed");
+		myList.Add ("Sofa");
+		myList.Add ("Table");
+		print ("My furniture: ");
+		//Loop through list and print furniture items
+		for (int i = 0; i < myList.Count; i++)
+		{
+			print(myList[i]);
+		}
+		print ("Adding Wardrobe to list...");
+		print ("Removing chair from list...");
+		//Add wardrobe to list
+		myList.Add ("Wardrobe");
+		//Remove Chair from list
+		myList.Remove("Chair");
+		//Loop through and print new list
+		print("New furniture list: ");
+		for (int i = 0; i < myList.Count; i++)
+		{
+			print(myList[i]);
+		}
+			
+	}
+
 }
+
+	//end tenth example
+
