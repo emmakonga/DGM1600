2ad69a3b708a64beef3b5f55bab5ae023198040f
Unknown <emmanymakonga@gmail.com>
2017-04-27 11:07:40 -0600

    Finish base calculator program

---
+++ DGM1600_Assignments/Assets/Scripts/CalcStaticVars.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalcStaticVars : MonoBehaviour {
+
+	public static int operation = 0;
+	public static int numA = 10;
+	public static int numB = 10;
+	public static bool entered = false;
+	// Use this for initialization
+	void Start () {
+		//numbers = new int[2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
